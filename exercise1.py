5f7e9c7a621b00a3aca8b126939673878d7238eb
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-15 19:57:55 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -2,9 +2,10 @@ a = int(input("Введите первое число = "))
 b = int(input("Введите второе число = "))
 c = int(input("Введите третье число = "))
 d = 5
-if (a and b<d) or (b and c<d) or (c and a<d):
-   print('yes')
-else:
-   print('no')
+if (a==b) or (a==c) or (c==b):
+        print('no')
+elif (a and b<d) or (a and c<d) or (b and c<d):
+        print('yes')
+
 
 
