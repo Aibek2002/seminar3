1df7c7524d45f50a5514f10ba886ce429535537e
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-16 12:29:36 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,20 +1,19 @@
-                                                
 print("Дано три числа. Если ровно два из них  меньше  5, то вывести yes, иначе вывести no.")
 
 try:
-        a = int(input("Введите первое число = "))
-        b = int(input("Введите второе число = "))
-        c = int(input("Введите третье число = "))
+   a = int(input("Введите первое число = "))
+   b = int(input("Введите второе число = "))
+   c = int(input("Введите третье число = "))
 except:
-        print("Tek san zhazynyz")
-
+   print("Tek san zhazynyz")
+   exit()
 if (a<5) and (b<5) and (c>5):
-        print("yes")
+   print("yes")
 elif (b<5) and (c<5) and (a>5):
-        print("yes")
+   print("yes")
 elif (a<5) and (c<5) and (b>5):
-        print("yes")
+   print("yes")
 else:
-        print("no")
+   print("no")
 
 
