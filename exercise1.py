be6214a4b06c96721a22492b314a7a23dca892ec
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-16 12:19:51 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,11 +1,20 @@
-a = int(input("Введите первое число = "))
-b = int(input("Введите второе число = "))
-c = int(input("Введите третье число = "))
-d = 5
-if (a==b) or (a==c) or (c==b):
-        print('no')
-elif (a and b<d) or (a and c<d) or (b and c<d):
-        print('yes')
+                                                
+print("Дано три числа. Если ровно два из них  меньше  5, то вывести yes, иначе вывести no.")
 
+try:
+        a = int(input("Введите первое число = "))
+        b = int(input("Введите второе число = "))
+        c = int(input("Введите третье число = "))
+except:
+        print("Tek san zhazynyz")
+
+if (a<5) and (b<5) and (c>5):
+        print("yes")
+elif (b<5) and (c<5) and (a>5):
+        print("yes")
+elif (a<5) and (c<5) and (b>5):
+        print("yes")
+else:
+        print("no")
 
 
