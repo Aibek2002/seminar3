576bd04ae9017ce9d4b70b37ade8b7e7cdc5b060
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-22 12:06:16 +0500

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -6,3 +6,11 @@ while n >0:
 
 print("Finally, n =",n)
 
+print("Выведите на экран числа 1, 2, 3, 4, ..., 20.")
+n=20
+def aik(a):
+	while a >0:
+		print(a)
+		a = a-1
+aik(n)
+print("Finally, n =",n)
